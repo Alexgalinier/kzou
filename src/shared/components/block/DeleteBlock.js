f9907c92f90925c8e94ad31542719323823772fc
Alex Galinier <contact@alexgalinier.com>
2018-02-16 17:01:47 +0100

    Create DeleteBlock to use it in other ressource page

---
+++ src/shared/components/block/DeleteBlock.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Block from './Block';
+import DangerButton from './../button/DangerButton';
+import Paragraph from './../paragraph/Paragraph';
+
+export default ({ title, warningText, handleDelete, className }) => (
+  <Block title={ title } className={"_b1 _bc-red " + (className ? className : '')}>
+    <div className="_fx _items-center">
+      <div className="_grow">
+        <Paragraph>{warningText}</Paragraph>
+      </div>
+      <div>
+        <DangerButton title="Supprimer" onClick={handleDelete} />
+      </div>
+    </div>
+  </Block>
+);
