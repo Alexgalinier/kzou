5766378b74ad3aee9c21b02d5e2f31dfc6032635
Alex Galinier <contact@alexgalinier.com>
2018-02-16 18:15:05 +0100

    Remove unsused Block

--- src/app/chapters/students/student/student-deletion.js
+++ src/app/chapters/students/student/student-deletion.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Block, DeleteBlock } from 'shared/components';
+import { DeleteBlock } from 'shared/components';
 
 export default ({ handleDelete }) => (
   <DeleteBlock
